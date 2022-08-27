9804716345d5bafaed0f5d029bee6d6a90471a74
Shakhriddin Korakhanov <107884731+Shakhin97@users.noreply.github.com>
2022-08-27 21:36:36 +0200

    My first Work
    Hello World

---
+++ Hello.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World! I'm Shakhriddin")
+
+
+# In[11]:
+
+
+print("Guten Morgen")
+
+
+# In[6]:
+
+
+print("Guten Tag")
+
+
+# In[7]:
+
+
+print('Guten Tag Herr Arbeitgeber')
+
+
+# In[9]:
+
+
+print("Sehr geehrte Damen und Herren , ich bin Shakhriddin und ich bin 25 Jahre alt.")
+
+
+# In[10]:
+
+
+print('Ich komme aus Usbekistan')
+
+
+# In[ ]:
+
+
+
+
