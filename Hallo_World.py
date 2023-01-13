396e0f27180a0286508437866c4a62baea219767
Shakhriddin Korakhanov <107884731+Shakhin97@users.noreply.github.com>
2023-01-14 00:33:04 +0100

    Add files via upload

---
+++ Hallo_World.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[37]:
+
+
+print("Hallo Welt")
+
+
+# In[38]:
+
+
+a=55
+b=45
+c=(a-b)*(a+b)
+print(c)
+
+
+# In[39]:
+
+
+d=(a^2)-(b^2)
+print(d)
+
+
+# In[40]:
+
+
+x= ((a^2)-(2*a*b) + (b^2))
+print(x)
+
+
+# In[41]:
+
+
+print("""Ich heisse Shaxhin
+und ich bin 25 Jahre alt""")
+
+
+# In[42]:
+
+
+print("Ich will in der Zukunf \nDeveloper werden")
+
+
+# In[43]:
+
+
+x=int(input("Geben Sie erste Zahl ein: "))
+y=int(input("Geben Sie zweite Zahl ein: "))
+z= ((x*x) + (y*y))
+print(f"Die Quadratsumme der Zahlen {x} und {y} ist {z}")
+
+
+# In[ ]:
+
+
+
+
